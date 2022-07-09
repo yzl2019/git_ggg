a13c541e186fa17838877e2e4fc73861311d7ac0
yangzl <zhdeks@163.com>
2022-07-09 20:23:15 +0800

    创建模块

---
+++ yzl_erueka/src/main/java/yzl/com/BootApp.java
@@ -0,0 +1,20 @@
+package yzl.com;
+
+/**
+ * @ClassName BootApp
+ * @Description TODO
+ * @Author 杨志亮
+ * @Date 2022/7/9 20:22
+ * @Version 1.0
+ **/
+public class BootApp {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
