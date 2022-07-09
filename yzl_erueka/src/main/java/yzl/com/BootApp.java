bb44c523a1c483cbb4f2ee508171b7496a290a6d
yangzl <zhdeks@163.com>
2022-07-09 20:31:07 +0800

    dev分支

--- yzl_erueka/src/main/java/yzl/com/BootApp.java
+++ yzl_erueka/src/main/java/yzl/com/BootApp.java
@@ -9,12 +9,20 @@ package yzl.com;
  **/
 public class BootApp {
     private String name;
-
+    private String password;
     public String getName() {
         return name;
     }
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 }
